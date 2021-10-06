fcca7b1892fe609682485f5358029132d614ba81
Megha <meghajagadale99@gmail.com>
2021-10-06 16:31:46 +0530

    Addedd:simple Java Web Server

---
+++ src/test/java/com/bridgelabz/SimpleHttpServer/HandlersTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.SimpleHttpServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandlersTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
